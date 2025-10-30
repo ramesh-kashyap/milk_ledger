bbe15202d7c7e2c16cd1d712f6ef6b53adaf03a3
Sachink03 <prajapatsachin835@gmail.com>
2025-10-30 11:18:59 +0530

    sachuin

--- lib/Screens/HomeScreen/view_transection.dart
+++ lib/Screens/HomeScreen/view_transection.dart
@@ -50,7 +50,11 @@ class _TransactionPageState extends State<TransactionPage> {
         }
       };
 
-      final response = await ApiService.post("/transection", body);
+      final response = await ApiService.post("/transection",{
+            ...body, // include existing fields
+            "from": fromDate.toIso8601String(),
+            "to": toDate.toIso8601String(),
+          },);
       final data = response.data;
 
       // print("Products: ${data["products"]}");
